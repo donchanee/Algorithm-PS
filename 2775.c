8f5208d5e696166507e761f83b85f144f6f77ff3
donchanee <42461455+donchanee@users.noreply.github.com>
2020-01-17 15:14:19 +0900

    Create 2775.c

---
+++ 2775.c
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/2775
+
+#include <stdio.h>
+
+int main() {
+	int t, n, k, d[15][15] = {0}, i, j;
+	
+	scanf("%d", &t);
+
+	for (i = 1; i < 15; i++) d[0][i] = i;
+	for (i = 1; i < 15; i++) {
+		for (j = 1; j < 15; j++) {
+			d[i][j] = d[i-1][j] + d[i][j-1];
+		}
+	}
+	
+	while (t--) {
+		scanf("%d %d", &k, &n);
+		printf("%d\n", d[k][n]);
+	}
+	return 0;
+}
