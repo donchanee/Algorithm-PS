831026817a736a528bea2208082c27cd2bcf332d
donchanee <42461455+donchanee@users.noreply.github.com>
2020-02-03 14:34:47 +0900

    분수 찾기

---
+++ 백준/수학1/1193.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int num = 1;
+    int n;
+    scanf("%d", &n);
+    
+    while(n - num > 0){
+        n -= num;
+        num++;
+    }
+    if(num % 2 == 1){
+        printf("%d/%d", num+1-n, n);
+    }else{
+        printf("%d/%d", n, num+1-n);
+    }
+}
