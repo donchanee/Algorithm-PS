efc24e014ff8f6770a5821d94d991e02420fc0a5
donchanee <42461455+donchanee@users.noreply.github.com>
2020-02-04 14:12:16 +0900

    직각삼각형

--- 백준/수학2/4153.py
+++ 백준/수학2/4153.py
@@ -4,7 +4,7 @@ while True:
     x,y,z = map(int, input().split())
     if x==0 and y==0 and z==0:
         break
-    if x*x+y*y == z*z:
+    if x*x+y*y == z*z or y*y+z*z == x*x or x*x+z*z == y*y:
         print('right')
     else:
         print('wrong')
