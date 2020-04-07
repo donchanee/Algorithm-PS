6cfe42671c64dcd5dcf84ca82e84d667622c503f
donchanee <dchan0305@gmail.com>
2020-04-07 13:58:44 +0900

    Update 1210.py

--- SWEA/1210.py
+++ SWEA/1210.py
@@ -21,3 +21,27 @@ for _ in range(10):
         if ladder[i][j] == 2:
             print("#",case," ",x,sep="")
             break
+
+'''
+for _ in range(10):
+    case = int(input())
+    max = 0
+    a = [list(map(int, input().split())) for _ in range(100)]
+
+    x = a[99].index(2)
+    y = 99
+
+    for _ in range(100):
+        if a[y][x-1] == 1 and x != 0:
+            a[y][x-1] = 0
+            x -= 1
+        elif a[y][x+1] == 1 and x != 99:
+            a[y][x+1] = 0
+            x += 1
+        else:
+            y -= 1
+
+    print("#",case," ",x,sep="")
+    
+    수정 필요.
+'''
