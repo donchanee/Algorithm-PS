1b673666347cb8c819fe3e044d56398e7dd9be26
donchanee <dchan0305@gmail.com>
2020-08-11 17:43:07 +0900

    Create 1463.py

---
+++ 백준/DP_동적계획법/1463.py
@@ -0,0 +1,22 @@
+N = int(input())
+count = 0
+R=[N]
+def cal(P):
+    li = []
+    for i in P:
+        li.append(i-1)
+        if i%3 == 0:
+            li.append(i//3)
+        if i%2 == 0:
+            li.append(i//2)
+    return li
+ 
+while N!=1:
+    temp = R[:]
+    R = []
+    R = cal(temp)
+    count +=1
+    if min(R) == 1:
+        break
+
+print(count)
