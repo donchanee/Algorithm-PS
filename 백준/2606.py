045b776e77b6278cbdd5e8f2260de0ee0e08f00b
donchanee <dchan0305@gmail.com>
2021-04-14 18:47:05 +0900

    Create 2606.py

---
+++ 백준/2606.py
@@ -0,0 +1,25 @@
+from collections import defaultdict, deque
+
+N = int(input())
+c = int(input())
+dic = defaultdict(list)
+visited = [0] * (N+1)
+
+for i in range(c):
+    a, b = map(int, input().split())
+    dic[a].append(b)
+    dic[b].append(a)
+
+q = deque()
+q.append(1)
+
+while q:
+    t = q.popleft()
+    for i in dic[t]:
+        if not visited[i]:
+            q.append(i)
+            visited[i] = 1
+
+print(sum(visited) - 1)
+
+# 단방향말고 양방향도 생각하기...
